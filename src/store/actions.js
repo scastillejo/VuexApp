9900f1d39483ab035a83cfc2d67a8955408a998f
Sebastian <it.buenosaires@hotmail.com>
2017-04-19 09:12:03 -0300

    minor change

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,19 +4,19 @@ export default {
     },
     findweather({ commit }, city) {
     	fetch('http://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=f695810da3d2519f537d79c03869977f&units=metric').then(  
-            function(response) {  
-              if (response.status !== 200) {  
-                console.log('Status Code: ' + response.status);  
-                return null;  
-              }  
-              response.json().then(function(data) {
-                commit('findweather', data);
-              });  
+          function(response) {  
+            if (response.status !== 200) {  
+              console.log('Status Code: ' + response.status);  
+              return null;  
             }  
-          )
-          .catch(function(err) {  
-            console.log(err);
-            return null;  
-          })
+            response.json().then(function(data) {
+              commit('findweather', data);
+            });  
+          }  
+        )
+        .catch(function(err) {  
+          console.log(err);
+          return null;  
+        })
     }
 }
